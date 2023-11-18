511235cb28fa655cc90180b3b4e50ea48f4b4c2a
zoto_ff <zoto_ff@vk.com>
2023-11-19 02:34:38 +0300

    return category index instead of name

--- src/classifier.py
+++ src/classifier.py
@@ -9,21 +9,16 @@ class RequestClassifier:
     categories = []
 
     def __init__(self, model_path=None):
-        project_dir = os.path.dirname(__file__) + '/../'
         model_path = model_path or (project_dir + 'data/model.pkl')
         self.model = joblib.load(model_path)
-
-        d = os.path.dirname(model_path)
-        with open(d + '/categories.json') as f:
-            self.categories = json.loads(f.read())
 
     def predict(self, text):
         data = self.model.predict([text])[0]
 
         confidence = np.max(self.model.predict_proba([text])[0])
         if confidence > 0.6:
-            category = self.categories[data[0]]
+            category = data[0]
         else:
-            category = 'contact_human_agent'
+            category = -1
 
         return category, data[1]
